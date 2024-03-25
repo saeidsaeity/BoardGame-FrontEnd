2575994e39c6a987059462b722096ad19ecd5fcc
saeidsaeity <saeidsaeity@outlook.com>
2024-03-25 11:59:15 +0000

    adds links

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+function Home() {
+    return (
+        <>
+        <Header/>
+        <h1>Welcome to Citizens of Newcastle</h1>
+        </>
+    )
+    
+}
+export default Home
---
+++ src/components/Login.jsx
@@ -0,0 +1,6 @@
+function Login() {
+    return(
+        <h1>Please Login</h1>
+    )
+}
+export default Login
