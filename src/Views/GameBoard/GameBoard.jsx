08e492df253ab9cc7c0843b3adc1a6bb87294094
Jay-Sparks <jay_sparks@icloud.com>
2024-03-25 23:13:49 +0000

    reducing 3d asset file size and including mesh modifiers

--- src/Views/GameBoard/GameBoard.jsx
+++ src/Views/GameBoard/GameBoard.jsx
@@ -1,37 +1,43 @@
 import { Canvas, useThree } from '@react-three/fiber'
-
-import styles from './GameBoard.module.css'
-
+import { OrbitControls } from '@react-three/drei'
 
 // asset loader
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 // import { useLoader } from '@react-three/fiber';
 // import { GLTFLoader } from 'three/addons';
 
 // 3D components
 import TileA from '../../assets/tiles/tileA.jsx'
+import TileB from '../../assets/tiles/tileB.jsx'
+import TileC from '../../assets/tiles/tileC.jsx'
 import TileD from '../../assets/tiles/tileD.jsx'
-import { OrbitControls } from '@react-three/drei'
+
+// styling
+import styles from './GameBoard.module.css'
 
 function GameBoard() {
-
+    
     // camera.rotation.set(0, 0.7, Math.PI/2); // Set camera rotation (in radians)
     // Store th  e initial z position
     //  camera.position.z = 100;
     
     // const tileD = useLoader(GLTFLoader,'/tileD.gltf')
     
-    const tileScale = [0.95, 0.95, 0.95]
+    const tileScale = [0.94, 0.94, 0.94]
 
     return(
         <div className={styles.gameBoard}>
-            <Canvas camera={{ fov: 60, position: [0, 5, 10]}}>
+            <Canvas camera={{ fov: 60, position: [0, 5, 10] }}>
                 <ambientLight/>
+                <directionalLight />
                 <OrbitControls 
                     minDistance={5} 
                     maxDistance={20} 
                 />
-                <TileD position={ [-1.50, 0 , -0.02] } scale={tileScale}/>
-                <TileA position={ [7.05, 0 , -2] } scale={tileScale} />
+                <TileA position={ [0, 0, -2] } scale={tileScale} />
+                <TileB position={ [ -2, 0, 0] } scale={tileScale} rotation-y={Math.PI * -0.5}/>
+                <TileC position={ [2, 0, 0] } scale={tileScale} rotation-y={Math.PI}/>
+                <TileD position={ [0, 0, 0] } scale={tileScale}/>
                     
                 <axesHelper args={[5]} />
                 <gridHelper args={[50, 25, 'black', 'red']} />
--- src/assets/tiles/tileA.jsx
+++ src/assets/tiles/tileA.jsx
@@ -1,34 +1,11 @@
-
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
-function TileA(props) {
+export default function TileA(props) {
   const { nodes, materials } = useGLTF('/tileA.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Cube010.geometry}
-        material={materials.Material}
-        position={[-7.403, 0.15, 0.02]}
-        scale={[0.95, 0.1, 0.95]}
-      />
-      <group position={[-7.403, 0.35, 0.02]} scale={[1, 0.1, 1]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Cube024.geometry}
-          material={materials['Material.001']}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Cube024_1.geometry}
-          material={materials['Stone_Light.001']}
-        />
-      </group>
-      <group position={[-7.421, 0.45, 0]} scale={0.2}>
+      <group position={[0, 0.45, 0]} scale={0.2}>
         <mesh
           castShadow
           receiveShadow
@@ -48,10 +25,30 @@ function TileA(props) {
           material={materials['Main.001']}
         />
       </group>
+      <group position={[0, 0.35, 0]} scale={[1, 0.1, 1]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube024.geometry}
+          material={materials['Material.001']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube024_1.geometry}
+          material={materials['Stone_Light.001']}
+        />
+      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube010.geometry}
+        material={materials['Material.002']}
+        position={[0, 0.15, 0]}
+        scale={[0.95, 0.1, 0.95]}
+      />
     </group>
   )
 }
 
 useGLTF.preload('/tileA.glb')
-
-export default TileA
