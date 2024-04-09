4da351bc3b775d54339c143e215ab4bd0c754ddd
saeidsaeity <saeidsaeity@outlook.com>
2024-04-09 21:18:52 +0100

    adds sound to the game

--- src/components/GameBoardCells/GameBoardCells.jsx
+++ src/components/GameBoardCells/GameBoardCells.jsx
@@ -1,23 +1,32 @@
-import * as THREE from 'three';
-import { tileChecks, tileColourLogic } from '../../../utils.js';
-import { useGameEngine } from '../../Context/useGameEngine.jsx';
-import { useContext } from 'react';
-import { BoardGameContext } from '../../Context/BoardGameContext.jsx';
-import { myPlayer } from 'playroomkit';
-
-export const GameBoardCells = ({
-
-  newTilePosition
-}) => {
+import * as THREE from "three";
+import { tileChecks, tileColourLogic } from "../../../utils.js";
+import { useGameEngine } from "../../Context/useGameEngine.jsx";
+import { useContext } from "react";
+import { BoardGameContext } from "../../Context/BoardGameContext.jsx";
+import { myPlayer } from "playroomkit";
+import useSound from "use-sound";
+export const GameBoardCells = ({ newTilePosition }) => {
+  const [sound] = useSound("drop.wav");
+  const [falling]= useSound('falling.mp3')
   const tileScale = [0.92, 0.92, 0.92];
   const tileSize = 2;
-  const{setReleaseTile,setNewTile2DPosition,setNewTileData,newTileMesh,
-    setNewTileMesh}=useContext(BoardGameContext)
-  const{setNewTilePosition,boardGameMatrix,turnPhase,isCitizenPhase, players,
-    playerTurn
+  const {
+    setReleaseTile,
+    setNewTile2DPosition,
+    setNewTileData,
+    newTileMesh,
+    setNewTileMesh,
+  } = useContext(BoardGameContext);
+  const {
+    setNewTilePosition,
+    boardGameMatrix,
+    turnPhase,
+    isCitizenPhase,
+    players,
+    playerTurn,
   } = useGameEngine();
-  const player = players[playerTurn]
-  const me = myPlayer()
+  const player = players[playerTurn];
+  const me = myPlayer();
   const grid = [];
   for (let i = -5; i < 6; i++) {
     for (let j = -5; j < 6; j++) {
@@ -27,74 +36,86 @@ export const GameBoardCells = ({
         <mesh
           key={`${i}-${j}-tile`}
           onClick={() => {
-            if(newTileMesh){
-            if(me.id === player.id){
-            if (turnPhase !== 'Place Tile') {
-              console.log('You can not place during the citizen phase!');
-              return [];
-            }
-            if (i === -5 || j === -5) {
-              // board edge case
-              if (
-                boardGameMatrix[i + 5][j + 5]?.length === 0 &&
-                (boardGameMatrix[i + 6][j + 5]?.length > 0 ||
-                  boardGameMatrix[i + 5][j + 6]?.length > 0 ||
-                  boardGameMatrix[i + 5][j + 4]?.length > 0)
-              ) {
-                tileChecks(
-                  position.x,
-                  position.z,
-                  i,
-                  j,
-                  setReleaseTile,
-                  setNewTilePosition,
-                  setNewTile2DPosition,
-                  tileSize,
-                  newTileMesh,
-                  setNewTileMesh
-                );
-                setNewTileData((currTileData) => {
-                  const newtilepos = { ...currTileData };
-                  newtilepos.grid_id = { row: position.x, column: position.z };
-                  return newtilepos;
-                });
+            if (newTileMesh) {
+              if (me.id === player.id) {
+                if (turnPhase !== "Place Tile") {
+                  console.log("You can not place during the citizen phase!");
+                  return [];
+                }
+                if (i === -5 || j === -5) {
+                  // board edge case
+                  if (
+                    boardGameMatrix[i + 5][j + 5]?.length === 0 &&
+                    (boardGameMatrix[i + 6][j + 5]?.length > 0 ||
+                      boardGameMatrix[i + 5][j + 6]?.length > 0 ||
+                      boardGameMatrix[i + 5][j + 4]?.length > 0)
+                  ) {
+                    falling()
+                    tileChecks(
+                      position.x,
+                      position.z,
+                      i,
+                      j,
+                      setReleaseTile,
+                      setNewTilePosition,
+                      setNewTile2DPosition,
+                      tileSize,
+                      newTileMesh,
+                      setNewTileMesh
+                    );
+                    setNewTileData((currTileData) => {
+                      const newtilepos = { ...currTileData };
+                      newtilepos.grid_id = {
+                        row: position.x,
+                        column: position.z,
+                      };
+                      
+                      setTimeout(function () {
+                        sound();
+                      }, 900);
+                      return newtilepos;
+                    });
+                  }
+                } else if (
+                  // selected a green tile
+                  boardGameMatrix[i + 5][j + 5]?.length === 0 &&
+                  (boardGameMatrix[i + 4][j + 5]?.length > 0 ||
+                    boardGameMatrix[i + 5][j + 4]?.length > 0 ||
+                    boardGameMatrix[i + 6][j + 5]?.length > 0 ||
+                    boardGameMatrix[i + 5][j + 6]?.length > 0)
+                ) {
+                  falling()
+                  //setReleaseTile(!releaseTile);
+                  tileChecks(
+                    position.x,
+                    position.z,
+                    i,
+                    j,
+                    setReleaseTile,
+                    setNewTilePosition,
+                    setNewTile2DPosition,
+                    tileSize,
+                    newTileMesh,
+                    setNewTileMesh
+                  );
+                  setNewTileData((currTileData) => {
+                    const newtilepos = { ...currTileData };
+                    newtilepos.grid_id = {
+                      row: position.x + 5,
+                      column: position.z + 5,
+                    };
+                    
+                    setTimeout(function () {
+                      sound();
+                    }, 1000);
+                    return newtilepos;
+                  });
+                }
               }
-            } else if (
-              // selected a green tile
-              boardGameMatrix[i + 5][j + 5]?.length === 0 &&
-              (boardGameMatrix[i + 4][j + 5]?.length > 0 ||
-                boardGameMatrix[i + 5][j + 4]?.length > 0 ||
-                boardGameMatrix[i + 6][j + 5]?.length > 0 ||
-                boardGameMatrix[i + 5][j + 6]?.length > 0)
-            ) {
-              //setReleaseTile(!releaseTile);
-              tileChecks(
-                position.x,
-                position.z,
-                i,
-                j,
-                setReleaseTile,
-                setNewTilePosition,
-                setNewTile2DPosition,
-                tileSize,
-                newTileMesh,
-                setNewTileMesh
-              );
-              setNewTileData((currTileData) => {
-                const newtilepos = { ...currTileData };
-                newtilepos.grid_id = {
-                  row: position.x + 5,
-                  column: position.z + 5,
-                };
-                return newtilepos;
-              });
+            } else {
+              console.log("please draw a tile");
             }
           }}
-          else{
-            console.log('please draw a tile');
-          }
-        }
-        }
           position={[i * tileSize, 0, j * tileSize]}
           scale={tileScale}
         >
